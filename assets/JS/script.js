ee07489f8ce0b16125eacbd8b5869b8bf8f2b83d
RengarLOL <joshuataldrich@gmail.com>
2022-07-20 16:58:24 -0400

    changes to javascript

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -18,12 +18,24 @@ var two = $("#2pm");
 var three = $("#3pm");
 var four = $("#4pm");
 var five = $("#5pm");
+var today = new Date();
+let times = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
+times.forEach(time => {
+    let form=document.createElement("form");
+    form.setAttribute("id", time );
+    form.classList.add("row");
+    form.innerHTML = `<span>${time}</span><textarea></textarea><button type="submit"><img src="https://img.icons8.com/ios/50/000000/save--v1.png"/></button>`;
+    document.querySelector(".container").appendChild(form);
+});
+
+
+console.log();
 
 /*Current Date*/
-var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+var myDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").html(myDate);
 
-//jumbotron date
-var Date = function() {
-    $("#currentDay").html(currentDate);
-}
+
+
+console.log(myDate);
 
