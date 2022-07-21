63fd8d12e929aea9f62fd00e7c5783557837d8dc
RengarLOL <joshuataldrich@gmail.com>
2022-07-21 00:09:03 -0400

    made changes to html to create my form outline instead of javascript

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -19,39 +19,35 @@ var three = $("#3pm");
 var four = $("#4pm");
 var five = $("#5pm");
 var today = new Date();
-let times = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
-times.forEach(time => {
-    var newTime = time.replace(/[^0-9]/g,'');
-    let momentTime = moment().format('LT'); 
-    momentTime = momentTime.split(":")[0];
-    let className = "";
-    if(time == momentTime) className="present";
-    else if (time < momentTime) className="past";
-    else className="future";
-    console.log(momentTime,newTime);
-    
-    
-     
-    let form = document.createElement("form");
-    form.setAttribute("id", time );
-    form.classList.add("row", className);
-    form.innerHTML = `<span class="col-2">${time}</span><textarea class="col-9"></textarea><button type="submit" class="col-1"><img src="https://img.icons8.com/ios/50/000000/save--v1.png"/></button>`;
-    document.querySelector(".container").appendChild(form);
-});
 
-let store = JSON.parse(localStorage.getItem("time"));
-let timeData = [];
-console.log(store);
-if (store === null)store = localStorage.setItem("time", JSON.stringify([]));
-else timeData = store;
+// let times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+// times.forEach((time) => {
+//   var newTime = time.replace(/[^0-9]/g, "");
+//   let momentTime = moment().format("LT");
+//   momentTime = momentTime.split(":")[0];
+//   let className = "";
+//   if (time == momentTime) className = "present";
+//   else if (time < momentTime) className = "past";
+//   else className = "future";
+//   console.log(momentTime, newTime);
 
-console.log();
+//   let form = document.createElement("form");
+//   form.setAttribute("id", time);
+//   form.classList.add("row", className);
+//   form.innerHTML = `<span class="col-2">${time}</span><textarea class="col-9"></textarea><button type="submit" class="col-1"><img src="https://img.icons8.com/ios/50/000000/save--v1.png"/></button>`;
+//   document.querySelector(".container").appendChild(form);
+// });
 
-/*Current Date*/
-var myDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").html(myDate);
+// let store = JSON.stringify(localStorage.getItem("time"));
+// let timeData = [];
+// console.log(store);
+// if (store === null) store = localStorage.setItem("time", JSON.stringify([]));
+// else timeData = store;
 
+// // console.log();
 
+// /*Current Date*/
+// var myDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+// $("#currentDay").html(myDate);
 
-console.log(myDate);
-
+// console.log(myDate);
